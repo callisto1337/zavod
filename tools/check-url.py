60404a57fc183372fde4eff42cc4e2482bfce4a4
callisto1337 <hazardous333@gmail.com>
2016-07-21 19:38:43 +0300

    tool for check url

---
+++ tools/check-url.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+import requests
+
+root = ET.parse('/home/mikhail/web4/sitemap.xml').getroot()
+counter, successes, errors = 0, 0, 0
+urls = []
+
+while counter < len(root):
+    urls.append(root[counter][0].text)
+    counter += 1
+counter = 0
+
+for n in urls:
+    counter += 1
+    r = requests.get(n)
+    if r.status_code != 200:
+        print(r.status_code)
+        errors += 1
+    if r.status_code == 200:
+        print('Success: ',  counter)
+        successes += 1
+
+print('All url: ', len(urls))
+print('Errors: ', errors)
+print('Successes: ', successes)
+
