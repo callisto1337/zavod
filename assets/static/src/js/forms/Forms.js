2a03106c82f3e9ed85c69807771e76ca8335badd
callisto1337 <hazardous333@gmail.com>
2016-09-17 22:56:14 +0300

    added template for form

---
+++ assets/static/src/js/forms/Forms.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-login').onclick = function() {
+    document.getElementById('form_bg').style.display = 'block';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('form_bg') || event.target == document.getElementsByClassName('form__close')[0])
+    {
+        document.getElementById('form_bg').style.display = "none";
+    }
+}
