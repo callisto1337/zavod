42772989bff7c96c7c0f23baac80b60b1a655d60
callisto1337 <hazardous333@gmail.com>
2016-09-18 20:16:40 +0300

    added form of search

--- assets/static/src/js/forms/Forms.js
+++ assets/static/src/js/forms/Forms.js
@@ -8,3 +8,14 @@ window.onclick = function(event) {
         document.getElementById('form_bg').style.display = "none";
     }
 }
+
+
+// Form of search
+document.getElementById('btn-search').onclick = function() {
+    document.getElementById('form-search').className = 'b-menu b-menu_search b-menu_search_toggled';
+}
+
+document.getElementById('form__button-cancel').onclick = function() {
+    document.getElementById('form-search').className = 'b-menu b-menu_search';
+}
+
