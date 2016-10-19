edc72dbbf2cf3ab5fbf356fd90c7bbc40d46fbe2
megge <ria6@yandex.ru>
2016-10-19 16:47:20 +0300

    black friday, faq, related products

--- zavod/constants.py
+++ zavod/constants.py
@@ -4,3 +4,12 @@ SPECIAL_FILTER_PARAMS = {
     u'Диапазон температур_max': ur'- ([\-, \+]\d+)°C',
     u'Диапазон температур_min': ur'([\-, \+]\d+)°C - ',
 }
+
+HIT = 0
+SALE = 1
+NEW = 2
+POPULAR_TYPE_CHOICES = (
+    (HIT, u'Хит'),
+    (SALE, u'Скидка!'),
+    (NEW, u'Новинка'),
+)
